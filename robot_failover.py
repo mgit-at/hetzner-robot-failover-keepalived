095e3a9a2cb1d5b8a1da69aa9287aef6de483798
Maciej Krüger <mkrueger@mgit.at>
2024-03-03 16:14:59 +0100

    Check if we really need to failover before sending request

--- robot_failover.py
+++ robot_failover.py
@@ -8,6 +8,7 @@ import requests
 from multiprocessing import Process
 from bunch import bunchify
 from base64 import b64encode
+from time import sleep
 
 CONFIG_PATH = os.path.join(os.path.abspath(
     os.path.dirname(__file__)), "config.json")
@@ -27,7 +28,8 @@ def add_ip(ip_bin_path, ip, interface):
         os.system(ip_bin_path + " addr add " + ip + "/32 dev " + interface)
 
 
-def change_request(endstate, url, header, payload, ip_bin_path, floating_ip, interface):
+def change_request(endstate, url, header, target_ip, ip_bin_path, floating_ip, interface):
+    log_prefix = "[%s -> %s] " % (url, target_ip)
     if endstate == "BACKUP":
         del_ip(ip_bin_path, floating_ip, interface)
     elif endstate == "FAULT":
@@ -36,21 +38,44 @@ def change_request(endstate, url, header, payload, ip_bin_path, floating_ip, int
     elif endstate == "MASTER":
         add_ip(ip_bin_path, floating_ip, interface)
         if header:
-            if payload:
-                print("Post request to: " + url)
-                print("Header: " + str(header))
-                print("Data: " + str(payload))
-                r = requests.post(url, data=payload, headers=header)
-                print("Response:")
+            while True:
+                current = requests.get(url, headers=header)
+                current = current.json()
+                payload = None
+
+                # we only need to specify the address if switching to *another* target
+                # if we switch to ourselves we need to send a delete request
+                if current['failover']['server_ip'] != target_ip and current['failover']['server_ipv6_net'] != target_ip:
+                    payload = "active_server_ip={}".format(target_ip)
+
+                if current['failover']['active_server_ip'] == target_ip:
+                    print(log_prefix + 'failed over as requested already, need no switch')
+                    break
+                elif not payload and not current['failover']['active_server_ip']:
+                    print(log_prefix + 'not failed over as request already, need no switch')
+                    break
+
+                r = None
+
+                if payload:
+                    print(log_prefix + "Post request to: " + url)
+                    print(log_prefix + "Header: " + str(header))
+                    print(log_prefix + "Data: " + str(payload))
+                    r = requests.post(url, data=payload, headers=header)
+                else:
+                    print(log_prefix + "Delete request to: " + url)
+                    print(log_prefix + "Header: " + str(header))
+                    r = requests.delete(url, headers=header)
+                print(log_prefix + "Response:")
                 print(r.status_code, r.reason)
                 print(r.text)
-            else:
-                print("Delete request to: " + url)
-                print("Header: " + str(header))
-                r = requests.delete(url, headers=header)
-                print("Response:")
-                print(r.status_code, r.reason)
-                print(r.text)#
+                j = r.json()
+                if r.status_code != 409 or j['error']['code'] != 'FAILOVER_LOCKED':
+                    break
+                else:
+                    print(log_prefix + 'trying again...')
+                    sleep(5)
+
     else:
         print("Error: Endstate not defined!")
 
@@ -91,17 +116,11 @@ def main(arg_vrouter, arg_type, arg_name, arg_endstate):
                 # this sets the headers for making a request to robot api
                 # which is not required when only switching vlan ips
                 header = {
-                    "Content-Type": "application/json",
+                    "Content-Type": "application/x-www-form-urlencoded",
                     "Authorization": "Basic " + b64encode(bytes(auth, 'utf-8')).decode('utf-8')
                 }
 
-
-            # we only need to specify the address if switching to *another* target
-            # if we switch to ourselves we need to send a delete request
-            if owner != config.this_router_id:
-                payload_floating = "active_server_ip={}".format(our)
-
-            Process(target=change_request, args=(arg_endstate, url, header, payload_floating,
+            Process(target=change_request, args=(arg_endstate, url, header, our,
                                              config.iproute2_bin, addr, config.interface)).start()
 
 if __name__ == "__main__":
