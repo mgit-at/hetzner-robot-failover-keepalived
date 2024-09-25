b7320e984c1dc8670d8049c310804a2d024d51ee
Maciej Krüger <mkrueger@mgit.at>
2024-09-25 16:53:41 +0200

    deb: add versioning

--- deb.sh
+++ deb.sh
@@ -4,7 +4,8 @@
 set -euxo pipefail
 
 PY_VER=$(head -n 1 robot_failover.pex  | grep -o "python3.*")
+PKG_VER=$(git describe --tags | sed "s|v||g")
 
 rm -fv *.deb
-fpm -s dir -v "0.0.0-$(git rev-parse --short HEAD)" -d "$PY_VER" -t deb -n robot-failover --prefix /usr/bin robot_failover.pex=robot_failover
+fpm -s dir -v "$PKG_VER" -d "$PY_VER" -t deb -n robot-failover --prefix /usr/bin robot_failover.pex=robot_failover
 mv *.deb robot_failover.deb
