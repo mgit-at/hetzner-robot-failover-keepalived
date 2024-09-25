06f2f5f5cef7081f964b129bd1e9e057f848025b
Maciej Krüger <mkrueger@mgit.at>
2024-09-25 16:43:54 +0200

    ci: use specific python version for deb

--- deb.sh
+++ deb.sh
@@ -3,6 +3,8 @@
 
 set -euxo pipefail
 
+PY_VER=$(head -n 1 robot_failover.pex  | grep -o "python3.*")
+
 rm -fv *.deb
-fpm -s dir -t deb -n robot-failover --prefix /usr/bin robot_failover.pex
+fpm -s dir -v "0.0.0-$(git rev-parse --short HEAD)" -d "$PY_VER" -t deb -n robot-failover --prefix /usr/bin robot_failover.pex=robot_failover
 mv *.deb robot_failover.deb
--- pex.sh
+++ pex.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p poetry
+#! nix-shell -i bash -p poetry -p python311
 
 set -euxo pipefail
 
 poetry install
 poetry run pip freeze > requirements.txt
-poetry run pex . -o robot_failover.pex -e robot_failover:main -r requirements.txt
+poetry run pex . -o robot_failover.pex -e robot_failover:main -r requirements.txt --python python3.11
