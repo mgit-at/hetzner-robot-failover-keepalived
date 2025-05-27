4c6e91f0ee180977085b752a71a5836f5c9ea58f
Maciej Krüger <mkrueger@mgit.at>
2025-05-27 22:26:43 +0200

    ci: build with python 3.13 and 3.11

--- pex.sh
+++ pex.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p poetry -p python311
+#! nix-shell -i bash -p poetry -p python311 -p python313
 
 set -euxo pipefail
 
 poetry install
 poetry run pip freeze > requirements.txt
-poetry run pex . -o robot_failover.pex -e robot_failover:cli -r requirements.txt --python python3.11
+poetry run pex . -o robot_failover.pex -e robot_failover:cli -r requirements.txt --python python3.11 --python python3.13 --python-shebang "/usr/bin/env python3"
